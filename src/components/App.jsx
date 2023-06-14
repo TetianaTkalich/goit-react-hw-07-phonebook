d0515ada702d7aaefbeb80f16f0d5f4c5b8f7ae3
TetianaTkalich <114866505+TetianaTkalich@users.noreply.github.com>
2023-06-14 21:20:10 +0300

    HW12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+import { useSelector } from 'react-redux';
+import ContactForm from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export default function App() {
+  const filter = useSelector(state => state.filter.value);
+  const contacts = useSelector(state => state.contacts.items);
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLocaleLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{ padding: '50px' }}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList contacts={filteredContacts} />
     </div>
   );
-};
+}
